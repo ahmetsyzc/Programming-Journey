9e442cd9191996e2bf9d6988d1a7a7d81e393301
ahmetyazici <ahmet@gmail.com>
2026-01-30 18:24:54 +0300

    Paint Projesi Comit İşlemi

---
+++ Paint Uygulaması/Paint Projesi/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Paint_Projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pen Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(139, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 46);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(267, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 46);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(203, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(58, 45);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(395, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 46);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(459, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 46);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Back Color";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(331, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(58, 45);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(842, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(58, 46);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(586, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(58, 46);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.Location = new System.Drawing.Point(650, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(58, 46);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.Location = new System.Drawing.Point(714, 12);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(58, 46);
+            this.button9.TabIndex = 10;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button10.Location = new System.Drawing.Point(778, 13);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(58, 46);
+            this.button10.TabIndex = 11;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 644);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Paint Uygulaması/Paint Projesi/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Paint_Projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true; // Ekran titremesini azaltır
+            this.Paint += Form1_Paint;
+        }
+
+        enum Arac { Kalem, Kare, Daire, Cizgi }
+        Arac seciliArac = Arac.Kalem;
+
+        bool ciziliyor = false;
+        bool silgiModu = false;
+
+        int baslangicX, baslangicY;
+        int kalemKalinlik = 3;
+        int silgiKalinlik = 3;
+
+        ColorDialog renkSecici = new ColorDialog();
+        Bitmap tuval;
+        Graphics cizim;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (tuval != null)
+                e.Graphics.DrawImage(tuval, 0, 0);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ciziliyor = true;
+            baslangicX = e.X;
+            baslangicY = e.Y;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!ciziliyor) return;
+
+            if (seciliArac == Arac.Kalem)
+            {
+                if (silgiModu)
+                {
+                    cizim.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+                    using (Pen p = new Pen(Color.Transparent, silgiKalinlik))
+                    {
+                        p.StartCap = p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                        cizim.DrawLine(p, baslangicX, baslangicY, e.X, e.Y);
+                    }
+                }
+                else
+                {
+                    cizim.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
+                    using (Pen p = new Pen(renkSecici.Color, kalemKalinlik))
+                    {
+                        p.StartCap = p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                        cizim.DrawLine(p, baslangicX, baslangicY, e.X, e.Y);
+                    }
+                }
+
+                baslangicX = e.X;
+                baslangicY = e.Y;
+                this.Invalidate();
+            }
+            else
+            {
+                this.Invalidate(); // Önceki önizlemeyi temizle
+
+                using (Graphics g = this.CreateGraphics())
+                {
+                    Pen p = new Pen(renkSecici.Color, kalemKalinlik);
+
+                    int x = Math.Min(baslangicX, e.X);
+                    int y = Math.Min(baslangicY, e.Y);
+                    int genislik = Math.Abs(e.X - baslangicX);
+                    int yukseklik = Math.Abs(e.Y - baslangicY);
+
+                    if (seciliArac == Arac.Kare)
+                        g.DrawRectangle(p, x, y, genislik, yukseklik);
+                    else if (seciliArac == Arac.Daire)
+                        g.DrawEllipse(p, x, y, genislik, yukseklik);
+                    else if (seciliArac == Arac.Cizgi)
+                        g.DrawLine(p, baslangicX, baslangicY, e.X, e.Y);
+                }
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (ciziliyor && seciliArac != Arac.Kalem)
+            {
+                using (Pen p = new Pen(renkSecici.Color, kalemKalinlik))
+                {
+                    int x = Math.Min(baslangicX, e.X);
+                    int y = Math.Min(baslangicY, e.Y);
+                    int genislik = Math.Abs(e.X - baslangicX);
+                    int yukseklik = Math.Abs(e.Y - baslangicY);
+
+                    if (seciliArac == Arac.Kare)
+                        cizim.DrawRectangle(p, x, y, genislik, yukseklik);
+                    else if (seciliArac == Arac.Daire)
+                        cizim.DrawEllipse(p, x, y, genislik, yukseklik);
+                    else if (seciliArac == Arac.Cizgi)
+                        cizim.DrawLine(p, baslangicX, baslangicY, e.X, e.Y);
+                }
+
+                this.Invalidate();
+            }
+
+            ciziliyor = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            seciliArac = Arac.Kalem;
+            silgiModu = false;
+            renkSecici.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            seciliArac = Arac.Kalem;
+            silgiModu = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            silgiModu = true;
+            seciliArac = Arac.Kalem;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cizim.Clear(Color.Transparent); // Sadece çizimleri siler
+            this.Invalidate();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            seciliArac = Arac.Kare;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            seciliArac = Arac.Daire;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            seciliArac = Arac.Cizgi;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG Dosyası|*.png|JPG Dosyası|*.jpg";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap cikti = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+                Graphics g = Graphics.FromImage(cikti);
+
+                g.Clear(this.BackColor);
+
+                // Arka plan resmi varsa onu da çiz
+                if (this.BackgroundImage != null)
+                    g.DrawImage(this.BackgroundImage, 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+
+                // Çizimleri ekle
+                g.DrawImage(tuval, 0, 0);
+
+                cikti.Save(sfd.FileName);
+                g.Dispose();
+                cikti.Dispose();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            kalemKalinlik = Convert.ToInt32(comboBox1.SelectedItem);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            silgiKalinlik = Convert.ToInt32(comboBox2.SelectedItem);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ColorDialog arkaPlanRenk = new ColorDialog();
+            if (arkaPlanRenk.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = arkaPlanRenk.Color;
+                this.BackgroundImage = null; // Sadece arka plan resmini siler
+                this.Invalidate();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosyaAc = new OpenFileDialog();
+            dosyaAc.Filter = "Resim Dosyaları|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (dosyaAc.ShowDialog() == DialogResult.OK)
+            {
+                Image yuklenenResim = Image.FromFile(dosyaAc.FileName);
+                this.BackgroundImage = new Bitmap(yuklenenResim, this.ClientSize);
+                this.BackgroundImageLayout = ImageLayout.Stretch;
+                this.Invalidate();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            object[] kalinliklar = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29 };
+            comboBox1.Items.AddRange(kalinliklar);
+            comboBox2.Items.AddRange(kalinliklar);
+
+            comboBox1.SelectedIndex = 1;
+            comboBox2.SelectedIndex = 1;
+
+            tuval = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            cizim = Graphics.FromImage(tuval);
+            cizim.Clear(Color.Transparent);
+        }
+    }
+}
